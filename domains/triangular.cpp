7e9565d1e4679fd8c826bc5e30b159f21bd3879a
Jonan CM <jonancm@gmail.com>
2013-06-10 14:48:37 +0100

    Reorder class definitions so that they appear in the same order in all files.

--- domains/triangular.cpp
+++ domains/triangular.cpp
@@ -1,26 +1,35 @@
 #include "triangular.hpp"
 
-TriangularSpherical3D_Domain::TriangularSpherical3D_Domain()
+// Triangle, cartesian 2D
+
+TriangularCartesian2D_Domain::TriangularCartesian2D_Domain()
 {
 	
 }
 
-TriangularPolar2D_Domain::TriangularPolar2D_Domain()
-{
-	
-}
-
-TriangularCylindrical3D_Domain::TriangularCylindrical3D_Domain()
-{
-	
-}
+// Triangle, cartesian 3D
 
 TriangularCartesian3D_Domain::TriangularCartesian3D_Domain()
 {
 	
 }
 
-TriangularCartesian2D_Domain::TriangularCartesian2D_Domain()
+// Triangle, cylindrical (3D)
+
+TriangularCylindrical3D_Domain::TriangularCylindrical3D_Domain()
+{
+	
+}
+
+// Triangle, polar (2D)
+
+TriangularPolar2D_Domain::TriangularPolar2D_Domain()
+{
+	
+}
+// Triangle, spherical (3D)
+
+TriangularSpherical3D_Domain::TriangularSpherical3D_Domain()
 {
 	
 }
--- domains/triangular.hpp
+++ domains/triangular.hpp
@@ -3,11 +3,15 @@
 
 #include "types.hpp"
 
+// Triangle, cartesian 2D
+
 class TriangularCartesian2D_Domain {
 	TriangularCartesian2D_Domain_t domain;
 public:
 	TriangularCartesian2D_Domain();
 };
+
+// Triangle, cartesian 3D
 
 class TriangularCartesian3D_Domain {
 	TriangularCartesian3D_Domain_t domain;
@@ -15,17 +19,23 @@ public:
 	TriangularCartesian3D_Domain();
 };
 
+// Triangle, cylindrical (3D)
+
 class TriangularCylindrical3D_Domain {
 	TriangularCylindrical3D_Domain_t domain;
 public:
 	TriangularCylindrical3D_Domain();
 };
 
+// Triangle, polar (2D)
+
 class TriangularPolar2D_Domain {
 	TriangularPolar2D_Domain_t domain;
 public:
 	TriangularPolar2D_Domain();
 };
+
+// Triangle, spherical (3D)
 
 class TriangularSpherical3D_Domain {
 	TriangularSpherical3D_Domain_t domain;
