7b966689c9e5f6f34e86a21dd17dc41dc6115e51
Jonan CM <jonancm@gmail.com>
2013-07-03 15:54:27 +0100

    Fix 'Domain.add_vertex' after removing old attributes and methods.

--- domains/triangular.cpp
+++ domains/triangular.cpp
@@ -42,11 +42,6 @@ unsigned int TriangularCartesian3D_Domain::num_vertices()
 void TriangularCartesian3D_Domain::add_vertex(PointCartesian3D vertex)
 {
 	viennagrid::create_element<TriangularCartesian3D_Vertex_t>(domain, vertex.get_point());
-
-	unsigned int point_id = num_vertices++;
-	TriangularCartesian3D_VertexRange_t vertex_range = viennagrid::elements<viennagrid::vertex_tag>(domain);
-	PointCartesian3D_t &point = viennagrid::point(domain, vertex_range[point_id]);
-	vertices.append<PointCartesian3D>(PointCartesian3D(&point, point_id));
 }
 
 TriangularCartesian3D_Domain_t & TriangularCartesian3D_Domain::get_domain()
@@ -74,11 +69,6 @@ unsigned int TriangularCylindrical3D_Domain::num_vertices()
 void TriangularCylindrical3D_Domain::add_vertex(PointCylindrical3D vertex)
 {
 	viennagrid::create_element<TriangularCylindrical3D_Vertex_t>(domain, vertex.get_point());
-
-	unsigned int point_id = num_vertices++;
-	TriangularCylindrical3D_VertexRange_t vertex_range = viennagrid::elements<viennagrid::vertex_tag>(domain);
-	PointCylindrical_t &point = viennagrid::point(domain, vertex_range[point_id]);
-	vertices.append<PointCylindrical3D>(PointCylindrical3D(point, point_id));
 }
 
 TriangularCylindrical3D_Domain_t & TriangularCylindrical3D_Domain::get_domain()
@@ -106,11 +96,6 @@ unsigned int TriangularPolar2D_Domain::num_vertices()
 void TriangularPolar2D_Domain::add_vertex(PointPolar2D vertex)
 {
 	viennagrid::create_element<TriangularPolar2D_Vertex_t>(domain, vertex.get_point());
-
-	unsigned int point_id = num_vertices++;
-	TriangularPolar2D_VertexRange_t vertex_range = viennagrid::elements<viennagrid::vertex_tag>(domain);
-	PointPolar_t &point = viennagrid::point(domain, vertex_range[point_id]);
-	vertices.append<PointPolar2D>(PointPolar2D(point, point_id));
 }
 
 TriangularPolar2D_Domain_t & TriangularPolar2D_Domain::get_domain()
@@ -138,11 +123,6 @@ unsigned int TriangularSpherical3D_Domain::num_vertices()
 void TriangularSpherical3D_Domain::add_vertex(PointSpherical3D vertex)
 {
 	viennagrid::create_element<TriangularSpherical3D_Vertex_t>(domain, vertex.get_point());
-
-	unsigned int point_id = num_vertices++;
-	TriangularSpherical3D_VertexRange_t vertex_range = viennagrid::elements<viennagrid::vertex_tag>(domain);
-	PointSpherical_t &point = viennagrid::point(domain, vertex_range[point_id]);
-	vertices.append<PointSpherical3D>(PointSpherical3D(point, point_id));
 }
 
 TriangularSpherical3D_Domain_t & TriangularSpherical3D_Domain::get_domain()
