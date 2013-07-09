6db5e37208146c4772371d42a1a3ea3fae5a4020
Jonan CM <jonancm@gmail.com>
2013-07-09 11:49:04 +0100

    Fix renamed classes after updating to latest development version of ViennaGrid.

--- domains/types.hpp
+++ domains/types.hpp
@@ -14,123 +14,123 @@
  ***************/
 
 // Line, cartesian 1D
-typedef viennagrid::result_of::domain_config<viennagrid::line_tag,
-                                             PointCartesian1D_t,
-                                             viennagrid::storage::id_handle_tag>::type    LinearCartesian1D_DomainConfig;
-typedef viennagrid::result_of::domain<LinearCartesian1D_DomainConfig>::type               LinearCartesian1D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
+                                                          PointCartesian1D_t,
+                                                          viennagrid::storage::id_handle_tag>::type    LinearCartesian1D_DomainConfig;
+typedef viennagrid::domain_t<LinearCartesian1D_DomainConfig>                                           LinearCartesian1D_Domain_t;
 
 // Line, cartesian 2D
-typedef viennagrid::result_of::domain_config<viennagrid::line_tag,
-                                             PointCartesian2D_t,
-                                             viennagrid::storage::id_handle_tag>::type    LinearCartesian2D_DomainConfig;
-typedef viennagrid::result_of::domain<LinearCartesian2D_DomainConfig>::type               LinearCartesian2D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
+                                                          PointCartesian2D_t,
+                                                          viennagrid::storage::id_handle_tag>::type    LinearCartesian2D_DomainConfig;
+typedef viennagrid::domain_t<LinearCartesian2D_DomainConfig>                                           LinearCartesian2D_Domain_t;
 
 // Line, cartesian 3D
-typedef viennagrid::result_of::domain_config<viennagrid::line_tag,
-                                             PointCartesian3D_t,
-                                             viennagrid::storage::id_handle_tag>::type    LinearCartesian3D_DomainConfig;
-typedef viennagrid::result_of::domain<LinearCartesian3D_DomainConfig>::type               LinearCartesian3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
+                                                          PointCartesian3D_t,
+                                                          viennagrid::storage::id_handle_tag>::type    LinearCartesian3D_DomainConfig;
+typedef viennagrid::domain_t<LinearCartesian3D_DomainConfig>                                           LinearCartesian3D_Domain_t;
 
 // Line, cylindrical (3D)
-typedef viennagrid::result_of::domain_config<viennagrid::line_tag,
-                                             PointCylindrical_t,
-                                             viennagrid::storage::id_handle_tag>::type    LinearCylindrical3D_DomainConfig;
-typedef viennagrid::result_of::domain<LinearCylindrical3D_DomainConfig>::type             LinearCylindrical3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
+                                                          PointCylindrical_t,
+                                                          viennagrid::storage::id_handle_tag>::type    LinearCylindrical3D_DomainConfig;
+typedef viennagrid::domain_t<LinearCylindrical3D_DomainConfig>                                         LinearCylindrical3D_Domain_t;
 
 // Line, polar (2D)
-typedef viennagrid::result_of::domain_config<viennagrid::line_tag,
-                                             PointPolar_t,
-                                             viennagrid::storage::id_handle_tag>::type    LinearPolar2D_DomainConfig;
-typedef viennagrid::result_of::domain<LinearPolar2D_DomainConfig>::type                   LinearPolar2D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
+                                                          PointPolar_t,
+                                                          viennagrid::storage::id_handle_tag>::type    LinearPolar2D_DomainConfig;
+typedef viennagrid::domain_t<LinearPolar2D_DomainConfig>                                               LinearPolar2D_Domain_t;
 
 // Line, spherical (3D)
-typedef viennagrid::result_of::domain_config<viennagrid::line_tag,
-                                             PointSpherical_t,
-                                             viennagrid::storage::id_handle_tag>::type    LinearSpherical3D_DomainConfig;
-typedef viennagrid::result_of::domain<LinearSpherical3D_DomainConfig>::type               LinearSpherical3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
+                                                          PointSpherical_t,
+                                                          viennagrid::storage::id_handle_tag>::type    LinearSpherical3D_DomainConfig;
+typedef viennagrid::domain_t<LinearSpherical3D_DomainConfig>                                           LinearSpherical3D_Domain_t;
 
 /*******************
  * TRIANGULAR MESH *
  *******************/
 
 // Triangle, cartesian 2D
-typedef viennagrid::result_of::domain_config<viennagrid::triangle_tag,
-                                             PointCartesian2D_t,
-                                             viennagrid::storage::id_handle_tag>::type    TriangularCartesian2D_DomainConfig;
-typedef viennagrid::result_of::domain<TriangularCartesian2D_DomainConfig>::type           TriangularCartesian2D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::triangle_tag,
+                                                          PointCartesian2D_t,
+                                                          viennagrid::storage::id_handle_tag>::type    TriangularCartesian2D_DomainConfig;
+typedef viennagrid::domain_t<TriangularCartesian2D_DomainConfig>                                       TriangularCartesian2D_Domain_t;
 
 // Triangle, cartesian 3D
-typedef viennagrid::result_of::domain_config<viennagrid::triangle_tag,
-                                             PointCartesian3D_t,
-                                             viennagrid::storage::id_handle_tag>::type    TriangularCartesian3D_DomainConfig;
-typedef viennagrid::result_of::domain<TriangularCartesian3D_DomainConfig>::type           TriangularCartesian3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::triangle_tag,
+                                                          PointCartesian3D_t,
+                                                          viennagrid::storage::id_handle_tag>::type    TriangularCartesian3D_DomainConfig;
+typedef viennagrid::domain_t<TriangularCartesian3D_DomainConfig>                                       TriangularCartesian3D_Domain_t;
 
 // Triangle, cylindrical (3D)
-typedef viennagrid::result_of::domain_config<viennagrid::triangle_tag,
-                                             PointCylindrical_t,
-                                             viennagrid::storage::id_handle_tag>::type    TriangularCylindrical3D_DomainConfig;
-typedef viennagrid::result_of::domain<TriangularCylindrical3D_DomainConfig>::type         TriangularCylindrical3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::triangle_tag,
+                                                          PointCylindrical_t,
+                                                          viennagrid::storage::id_handle_tag>::type    TriangularCylindrical3D_DomainConfig;
+typedef viennagrid::domain_t<TriangularCylindrical3D_DomainConfig>                                     TriangularCylindrical3D_Domain_t;
 
 // Triangle, polar (2D)
-typedef viennagrid::result_of::domain_config<viennagrid::triangle_tag,
-                                             PointPolar_t,
-                                             viennagrid::storage::id_handle_tag>::type    TriangularPolar2D_DomainConfig;
-typedef viennagrid::result_of::domain<TriangularPolar2D_DomainConfig>::type               TriangularPolar2D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::triangle_tag,
+                                                          PointPolar_t,
+                                                          viennagrid::storage::id_handle_tag>::type    TriangularPolar2D_DomainConfig;
+typedef viennagrid::domain_t<TriangularPolar2D_DomainConfig>                                           TriangularPolar2D_Domain_t;
 
 // Triangle, spherical (3D)
-typedef viennagrid::result_of::domain_config<viennagrid::triangle_tag,
-                                             PointSpherical_t,
-                                             viennagrid::storage::id_handle_tag>::type    TriangularSpherical3D_DomainConfig;
-typedef viennagrid::result_of::domain<TriangularSpherical3D_DomainConfig>::type           TriangularSpherical3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::triangle_tag,
+                                                          PointSpherical_t,
+                                                          viennagrid::storage::id_handle_tag>::type    TriangularSpherical3D_DomainConfig;
+typedef viennagrid::domain_t<TriangularSpherical3D_DomainConfig>                                       TriangularSpherical3D_Domain_t;
 
 /**********************
  * QUADRILATERAL MESH *
  **********************/
 
 // Quadrilateral, cartesian 2D
-typedef viennagrid::result_of::domain_config<viennagrid::quadrilateral_tag,
-                                             PointCartesian2D_t,
-                                             viennagrid::storage::id_handle_tag>::type    QuadrilateralCartesian2D_DomainConfig;
-typedef viennagrid::result_of::domain<QuadrilateralCartesian2D_DomainConfig>::type        QuadrilateralCartesian2D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::quadrilateral_tag,
+                                                          PointCartesian2D_t,
+                                                          viennagrid::storage::id_handle_tag>::type    QuadrilateralCartesian2D_DomainConfig;
+typedef viennagrid::domain_t<QuadrilateralCartesian2D_DomainConfig>                                    QuadrilateralCartesian2D_Domain_t;
 // Quadrilateral, cartesian 3D
-typedef viennagrid::result_of::domain_config<viennagrid::quadrilateral_tag,
-                                             PointCartesian3D_t,
-                                             viennagrid::storage::id_handle_tag>::type    QuadrilateralCartesian3D_DomainConfig;
-typedef viennagrid::result_of::domain<QuadrilateralCartesian3D_DomainConfig>::type        QuadrilateralCartesian3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::quadrilateral_tag,
+                                                          PointCartesian3D_t,
+                                                          viennagrid::storage::id_handle_tag>::type    QuadrilateralCartesian3D_DomainConfig;
+typedef viennagrid::domain_t<QuadrilateralCartesian3D_DomainConfig>                                    QuadrilateralCartesian3D_Domain_t;
 // Quadrilateral, cylindrical (3D)
-typedef viennagrid::result_of::domain_config<viennagrid::quadrilateral_tag,
-                                             PointCylindrical_t,
-                                             viennagrid::storage::id_handle_tag>::type    QuadrilateralCylindrical3D_DomainConfig;
-typedef viennagrid::result_of::domain<QuadrilateralCylindrical3D_DomainConfig>::type      QuadrilateralCylindrical3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::quadrilateral_tag,
+                                                          PointCylindrical_t,
+                                                          viennagrid::storage::id_handle_tag>::type    QuadrilateralCylindrical3D_DomainConfig;
+typedef viennagrid::domain_t<QuadrilateralCylindrical3D_DomainConfig>                                  QuadrilateralCylindrical3D_Domain_t;
 // Quadrilateral, polar (2D)
-typedef viennagrid::result_of::domain_config<viennagrid::quadrilateral_tag,
-                                             PointPolar_t,
-                                             viennagrid::storage::id_handle_tag>::type    QuadrilateralPolar2D_DomainConfig;
-typedef viennagrid::result_of::domain<QuadrilateralPolar2D_DomainConfig>::type            QuadrilateralPolar2D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::quadrilateral_tag,
+                                                          PointPolar_t,
+                                                          viennagrid::storage::id_handle_tag>::type    QuadrilateralPolar2D_DomainConfig;
+typedef viennagrid::domain_t<QuadrilateralPolar2D_DomainConfig>                                        QuadrilateralPolar2D_Domain_t;
 // Quadrilateral, spherical (3D)
-typedef viennagrid::result_of::domain_config<viennagrid::quadrilateral_tag,
-                                             PointSpherical_t,
-                                             viennagrid::storage::id_handle_tag>::type    QuadrilateralSpherical3D_DomainConfig;
-typedef viennagrid::result_of::domain<QuadrilateralSpherical3D_DomainConfig>::type        QuadrilateralSpherical3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::quadrilateral_tag,
+                                                          PointSpherical_t,
+                                                          viennagrid::storage::id_handle_tag>::type    QuadrilateralSpherical3D_DomainConfig;
+typedef viennagrid::domain_t<QuadrilateralSpherical3D_DomainConfig>                                    QuadrilateralSpherical3D_Domain_t;
 
 /********************
  * TETRAHEDRAL MESH *
  ********************/
 
 // Tetrahedral, cartesian 3D
-typedef viennagrid::result_of::domain_config<viennagrid::tetrahedron_tag,
-                                             PointCartesian3D_t,
-                                             viennagrid::storage::id_handle_tag>::type    TetrahedralCartesian3D_DomainConfig;
-typedef viennagrid::result_of::domain<TetrahedralCartesian3D_DomainConfig>::type          TetrahedralCartesian3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::tetrahedron_tag,
+                                                          PointCartesian3D_t,
+                                                          viennagrid::storage::id_handle_tag>::type    TetrahedralCartesian3D_DomainConfig;
+typedef viennagrid::domain_t<TetrahedralCartesian3D_DomainConfig>                                      TetrahedralCartesian3D_Domain_t;
 // Tetrahedral, cylindrical (3D)
-typedef viennagrid::result_of::domain_config<viennagrid::tetrahedron_tag,
-                                             PointCylindrical_t,
-                                             viennagrid::storage::id_handle_tag>::type    TetrahedralCylindrical3D_DomainConfig;
-typedef viennagrid::result_of::domain<TetrahedralCylindrical3D_DomainConfig>::type        TetrahedralCylindrical3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::tetrahedron_tag,
+                                                          PointCylindrical_t,
+                                                          viennagrid::storage::id_handle_tag>::type    TetrahedralCylindrical3D_DomainConfig;
+typedef viennagrid::domain_t<TetrahedralCylindrical3D_DomainConfig>                                    TetrahedralCylindrical3D_Domain_t;
 // Tetrahedral, spherical (3D)
-typedef viennagrid::result_of::domain_config<viennagrid::tetrahedron_tag,
-                                             PointSpherical_t,
-                                             viennagrid::storage::id_handle_tag>::type    TetrahedralSpherical3D_DomainConfig;
-typedef viennagrid::result_of::domain<TetrahedralSpherical3D_DomainConfig>::type          TetrahedralSpherical3D_Domain_t;
+typedef viennagrid::config::result_of::full_domain_config<viennagrid::tetrahedron_tag,
+                                                          PointSpherical_t,
+                                                          viennagrid::storage::id_handle_tag>::type    TetrahedralSpherical3D_DomainConfig;
+typedef viennagrid::domain_t<TetrahedralSpherical3D_DomainConfig>                                      TetrahedralSpherical3D_Domain_t;
 
 #endif /* end of include guard: TYPES_HPP_O176Z3NS */
--- segments/types.hpp
+++ segments/types.hpp
@@ -8,72 +8,72 @@
  ***************/
 
 // Line, cartesian 1D
-typedef viennagrid::result_of::geometric_view<LinearCartesian1D_Domain_t>::type      LinearCartesian1D_Segment_t;
+typedef viennagrid::result_of::domain_view<LinearCartesian1D_Domain_t>::type      LinearCartesian1D_Segment_t;
 
 // Line, cartesian 2D
-typedef viennagrid::result_of::geometric_view<LinearCartesian2D_Domain_t>::type      LinearCartesian2D_Segment_t;
+typedef viennagrid::result_of::domain_view<LinearCartesian2D_Domain_t>::type      LinearCartesian2D_Segment_t;
 
 // Line, cartesian 3D
-typedef viennagrid::result_of::geometric_view<LinearCartesian3D_Domain_t>::type      LinearCartesian3D_Segment_t;
+typedef viennagrid::result_of::domain_view<LinearCartesian3D_Domain_t>::type      LinearCartesian3D_Segment_t;
 
 // Line, cylindrical (3D)
-typedef viennagrid::result_of::geometric_view<LinearCylindrical3D_Domain_t>::type    LinearCylindrical3D_Segment_t;
+typedef viennagrid::result_of::domain_view<LinearCylindrical3D_Domain_t>::type    LinearCylindrical3D_Segment_t;
 
 // Line, polar (2D)
-typedef viennagrid::result_of::geometric_view<LinearPolar2D_Domain_t>::type          LinearPolar2D_Segment_t;
+typedef viennagrid::result_of::domain_view<LinearPolar2D_Domain_t>::type          LinearPolar2D_Segment_t;
 
 // Line, spherical (3D)
-typedef viennagrid::result_of::geometric_view<LinearSpherical3D_Domain_t>::type      LinearSpherical3D_Segment_t;
+typedef viennagrid::result_of::domain_view<LinearSpherical3D_Domain_t>::type      LinearSpherical3D_Segment_t;
 
 /*******************
  * TRIANGULAR MESH *
  *******************/
 
 // Triangle, cartesian 2D
-typedef viennagrid::result_of::geometric_view<TriangularCartesian2D_Domain_t>::type      TriangularCartesian2D_Segment_t;
+typedef viennagrid::result_of::domain_view<TriangularCartesian2D_Domain_t>::type      TriangularCartesian2D_Segment_t;
 
 // Triangle, cartesian 3D
-typedef viennagrid::result_of::geometric_view<TriangularCartesian3D_Domain_t>::type      TriangularCartesian3D_Segment_t;
+typedef viennagrid::result_of::domain_view<TriangularCartesian3D_Domain_t>::type      TriangularCartesian3D_Segment_t;
 
 // Triangle, cylindrical (3D)
-typedef viennagrid::result_of::geometric_view<TriangularCylindrical3D_Domain_t>::type    TriangularCylindrical3D_Segment_t;
+typedef viennagrid::result_of::domain_view<TriangularCylindrical3D_Domain_t>::type    TriangularCylindrical3D_Segment_t;
 
 // Triangle, polar (2D)
-typedef viennagrid::result_of::geometric_view<TriangularPolar2D_Domain_t>::type          TriangularPolar2D_Segment_t;
+typedef viennagrid::result_of::domain_view<TriangularPolar2D_Domain_t>::type          TriangularPolar2D_Segment_t;
 
 // Triangle, spherical (3D)
-typedef viennagrid::result_of::geometric_view<TriangularSpherical3D_Domain_t>::type      TriangularSpherical3D_Segment_t;
+typedef viennagrid::result_of::domain_view<TriangularSpherical3D_Domain_t>::type      TriangularSpherical3D_Segment_t;
 
 /**********************
  * QUADRILATERAL MESH *
  **********************/
 
 // Quadrilateral, cartesian 2D
-typedef viennagrid::result_of::geometric_view<QuadrilateralCartesian2D_Domain_t>::type      QuadrilateralCartesian2D_Segment_t;
+typedef viennagrid::result_of::domain_view<QuadrilateralCartesian2D_Domain_t>::type      QuadrilateralCartesian2D_Segment_t;
 
 // Quadrilateral, cartesian 3D
-typedef viennagrid::result_of::geometric_view<QuadrilateralCartesian3D_Domain_t>::type      QuadrilateralCartesian3D_Segment_t;
+typedef viennagrid::result_of::domain_view<QuadrilateralCartesian3D_Domain_t>::type      QuadrilateralCartesian3D_Segment_t;
 
 // Quadrilateral, cylindrical (3D)
-typedef viennagrid::result_of::geometric_view<QuadrilateralCylindrical3D_Domain_t>::type    QuadrilateralCylindrical3D_Segment_t;
+typedef viennagrid::result_of::domain_view<QuadrilateralCylindrical3D_Domain_t>::type    QuadrilateralCylindrical3D_Segment_t;
 
 // Quadrilateral, polar (2D)
-typedef viennagrid::result_of::geometric_view<QuadrilateralPolar2D_Domain_t>::type          QuadrilateralPolar2D_Segment_t;
+typedef viennagrid::result_of::domain_view<QuadrilateralPolar2D_Domain_t>::type          QuadrilateralPolar2D_Segment_t;
 
 // Quadrilateral, spherical (3D)
-typedef viennagrid::result_of::geometric_view<QuadrilateralSpherical3D_Domain_t>::type      QuadrilateralSpherical3D_Segment_t;
+typedef viennagrid::result_of::domain_view<QuadrilateralSpherical3D_Domain_t>::type      QuadrilateralSpherical3D_Segment_t;
 
 /**********************
  * TETRAHEDRAL MESH *
  **********************/
 
 // Tetrahedral, cartesian 3D
-typedef viennagrid::result_of::geometric_view<TetrahedralCartesian3D_Domain_t>::type      TetrahedralCartesian3D_Segment_t;
+typedef viennagrid::result_of::domain_view<TetrahedralCartesian3D_Domain_t>::type      TetrahedralCartesian3D_Segment_t;
 
 // Tetrahedral, cylindrical (3D)
-typedef viennagrid::result_of::geometric_view<TetrahedralCylindrical3D_Domain_t>::type    TetrahedralCylindrical3D_Segment_t;
+typedef viennagrid::result_of::domain_view<TetrahedralCylindrical3D_Domain_t>::type    TetrahedralCylindrical3D_Segment_t;
 
 // Tetrahedral, spherical (3D)
-typedef viennagrid::result_of::geometric_view<TetrahedralSpherical3D_Domain_t>::type      TetrahedralSpherical3D_Segment_t;
+typedef viennagrid::result_of::domain_view<TetrahedralSpherical3D_Domain_t>::type      TetrahedralSpherical3D_Segment_t;
 
 #endif /* end of include guard: TYPES_HPP_LQVAVLAX */
