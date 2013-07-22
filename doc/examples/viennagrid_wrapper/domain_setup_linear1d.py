20165733873c9236ad6a42555d0b57539776304e
Jonan CM <jonancm@gmail.com>
2013-07-22 11:47:27 +0100

    Improve Python code examples.

---
+++ doc/examples/viennagrid_wrapper/domain_setup_linear1d.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from viennagrid_wrapper import LinearCartesian1D_Domain as Domain
+from viennagrid_wrapper import LinearCartesian1D_Segmentation as Segmentation
+from viennagrid_wrapper import PointCartesian1D as Point
+
+# Create a domain and add 4 vertices to it
+domain = Domain()
+domain.add_vertex(Point(1))
+domain.add_vertex(Point(2))
+domain.add_vertex(Point(3))
+domain.add_vertex(Point(4))
+
+# Create a segmentation on the domain with 2 segments in it
+segmentation = Segmentation(domain)
+seg0 = segmentation.create_segment()
+seg1 = segmentation.create_segment()
+
+# Create cell #1 in seg0
+v1 = domain.vertices[0] # alternatively: domain.get_vertex(0), which may exhibit an improved preformance
+v2 = domain.vertices[1] # alternatively: domain.get_vertex(1), which may exhibit an improved preformance
+cell = seg0.create_cell(v1, v2)
+# Create cell #2 in seg0
+v1 = domain.vertices[2] # alternatively: domain.get_vertex(2), which may exhibit an improved preformance
+v2 = domain.vertices[3] # alternatively: domain.get_vertex(3), which may exhibit an improved preformance
+cell = seg0.create_cell(v1, v2)
+
+# Create cell #1 in seg1
+v1 = domain.vertices[0] # alternatively: domain.get_vertex(0), which may exhibit an improved preformance
+v2 = domain.vertices[1] # alternatively: domain.get_vertex(1), which may exhibit an improved preformance
+cell = seg1.create_cell(v1, v2)
+# Create cell #2 in seg1
+v1 = domain.vertices[2] # alternatively: domain.get_vertex(2), which may exhibit an improved preformance
+v2 = domain.vertices[3] # alternatively: domain.get_vertex(3), which may exhibit an improved preformance
+cell = seg1.create_cell(v1, v2)
+
+print('\nDomain has', len(domain.vertices), 'vertices:\n')
+for i, vertex in enumerate(domain.vertices):
+	print('\tVertex ', i, ': ' , vertex, sep='')
+else:
+	print()
+
+print('Segmentation has', len(segmentation.segments), 'segments:\n')
+for i, segment in enumerate(segmentation.segments):
+	print('\tSegment seg', i, ' has ', len(segment.cells), ' cells:\n', sep='')
+	for cell in segment.cells:
+		print('\t\tCell has', len(cell.vertices), 'vertices:\n')
+		for vertex in cell.vertices:
+			print('\t\t\t', vertex)
+		else:
+			print()
+	else:
+		print()
