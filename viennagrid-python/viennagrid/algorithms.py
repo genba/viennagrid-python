3b42aa408472755ecb4c63d60f8bb14f43b4e930
Jonan CM <jonancm@gmail.com>
2013-07-29 13:04:34 +0100

    Write high-level code for vector-based algorithms (inner product, cross product and norm).

---
+++ viennagrid-python/viennagrid/algorithms.py
@@ -0,0 +1,73 @@
+#-*- coding: utf-8 -*-
+
+import viennagrid_wrapper as _wrapper
+
+_SUPPORTED_NORMS = (1, 2, 'inf')
+
+###########################
+# VECTOR-BASED ALGORITHMS #
+###########################
+
+def inner_prod(point1, point2):
+	# Try to get method 'inner_prod' from 'point1'. If it doesn't have the method,
+	# it means it's not a cartesian point. Thus, convert to cartesian coordinates
+	# and get the method. If it still doesn't have the method, raise an exception.
+	try:
+		inner_prod_fn = point1.__getattribute__('inner_prod')
+	except AttributeError:
+		casted_pnt1 = point1.to_cartesian()
+		try:
+			inner_prod_fn = casted_pnt1.__getattribute__('inner_prod')
+		except AttributeError:
+			raise TypeError('point1 has no method named inner_prod')
+	else:
+		casted_pnt1 = point1
+	
+	# If point types are equal, simply calculate the inner product. If they're not
+	# equal, try to convert 'point2' to the type of 'point1'. If types are still
+	# different, it means that both points are of incompatible types
+	# (i.e. incompatible dimensions).
+	if casted_pnt1.__class__ is point2.__class__:
+		return inner_prod_fun(point2)
+	else:
+		casted_pnt2 = point2.to_cartesian()
+		if casted_pnt1.__class__ is casted_pnt2.__class__:
+			return inner_prod_fun(casted_pnt2)
+		else:
+			raise TypeError('incompatible point types')
+
+def cross_prod(point1, point2):
+	# Try to get method 'cross_prod' from 'point1'. If it doesn't have the method,
+	# it means it's not a cartesian point. Thus, convert to cartesian coordinates
+	# and get the method. If it still doesn't have the method, raise an exception.
+	try:
+		cross_prod_fn = point1.__getattribute__('cross_prod')
+	except AttributeError:
+		casted_pnt1 = point1.to_cartesian()
+		try:
+			cross_prod_fn = casted_pnt1.__getattribute__('cross_prod')
+		except AttributeError:
+			raise TypeError('point1 has no method named cross_prod')
+	else:
+		casted_pnt1 = point1
+	
+	# If point types are equal, simply calculate the cross product. If they're not
+	# equal, try to convert 'point2' to the type of 'point1'. If types are still
+	# different, it means that both points are of incompatible types
+	# (i.e. incompatible dimensions).
+	if casted_pnt1.__class__ is point2.__class__:
+		return cross_prod_fn(point2)
+	else:
+		casted_pnt2 = point2.to_cartesian()
+		if casted_pnt1.__class__ is casted_pnt2.__class__:
+			return cross_prod_fn(casted_pnt2)
+		else:
+			raise TypeError('incompatible point types')
+
+def norm(point, norm_type=2):
+	if norm_type in _SUPPORTED_NORMS:
+		norm_type = str(norm_type)
+		norm_fn = _wrapper.__getattribute__('norm_%(norm_type)s' % locals())
+		return norm_fn(point)
+	else:
+		raise ValueError('unsupported norm type: %(norm_type)s')
