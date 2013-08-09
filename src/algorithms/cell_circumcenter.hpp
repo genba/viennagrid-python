b2d5292667bc3aa506a5a254e09398c9eed3de23
Jonan CM <jonancm@gmail.com>
2013-08-09 17:19:42 +0100

    Distribute cell based algorithms across several files.

---
+++ src/algorithms/cell_circumcenter.hpp
@@ -0,0 +1,44 @@
+#ifndef ALGORHITHMS_CELL_CIRCUMCENTER_HPP
+#define ALGORHITHMS_CELL_CIRCUMCENTER_HPP
+
+#include <boost/python.hpp>
+using namespace boost::python;
+
+#include "../points/cartesian.hpp"
+#include "../points/cylindrical.hpp"
+#include "../points/polar.hpp"
+#include "../points/spherical.hpp"
+
+#include "../cells/linear.hpp"
+#include "../cells/triangular.hpp"
+#include "../cells/quadrilateral.hpp"
+#include "../cells/tetrahedral.hpp"
+
+/****************
+ * CIRCUMCENTER *
+ ****************/
+
+PointCartesian1D LinearCartesian1D_Cell_circumcenter(LinearCartesian1D_Cell cell);
+PointCartesian2D LinearCartesian2D_Cell_circumcenter(LinearCartesian2D_Cell cell);
+PointCartesian3D LinearCartesian3D_Cell_circumcenter(LinearCartesian3D_Cell cell);
+PointCylindrical3D LinearCylindrical3D_Cell_circumcenter(LinearCylindrical3D_Cell cell);
+PointPolar2D LinearPolar2D_Cell_circumcenter(LinearPolar2D_Cell cell);
+PointSpherical3D LinearSpherical3D_Cell_circumcenter(LinearSpherical3D_Cell cell);
+
+PointCartesian2D TriangularCartesian2D_Cell_circumcenter(TriangularCartesian2D_Cell cell);
+PointCartesian3D TriangularCartesian3D_Cell_circumcenter(TriangularCartesian3D_Cell cell);
+PointCylindrical3D TriangularCylindrical3D_Cell_circumcenter(TriangularCylindrical3D_Cell cell);
+PointPolar2D TriangularPolar2D_Cell_circumcenter(TriangularPolar2D_Cell cell);
+PointSpherical3D TriangularSpherical3D_Cell_circumcenter(TriangularSpherical3D_Cell cell);
+
+PointCartesian2D QuadrilateralCartesian2D_Cell_circumcenter(QuadrilateralCartesian2D_Cell cell);
+PointCartesian3D QuadrilateralCartesian3D_Cell_circumcenter(QuadrilateralCartesian3D_Cell cell);
+PointCylindrical3D QuadrilateralCylindrical3D_Cell_circumcenter(QuadrilateralCylindrical3D_Cell cell);
+PointPolar2D QuadrilateralPolar2D_Cell_circumcenter(QuadrilateralPolar2D_Cell cell);
+PointSpherical3D QuadrilateralSpherical3D_Cell_circumcenter(QuadrilateralSpherical3D_Cell cell);
+
+PointCartesian3D TetrahedralCartesian3D_Cell_circumcenter(TetrahedralCartesian3D_Cell cell);
+PointCylindrical3D TetrahedralCylindrical3D_Cell_circumcenter(TetrahedralCylindrical3D_Cell cell);
+PointSpherical3D TetrahedralSpherical3D_Cell_circumcenter(TetrahedralSpherical3D_Cell cell);
+
+#endif
---
+++ src/algorithms/cell_surface.cpp
@@ -0,0 +1,110 @@
+#include "cell_surface.hpp"
+
+#include <viennagrid/algorithm/surface.hpp>
+
+/***********
+ * SURFACE *
+ ***********/
+
+// Linear
+ 
+double LinearCartesian1D_Cell_surface(LinearCartesian1D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double LinearCartesian2D_Cell_surface(LinearCartesian2D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double LinearCartesian3D_Cell_surface(LinearCartesian3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double LinearCylindrical3D_Cell_surface(LinearCylindrical3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double LinearPolar2D_Cell_surface(LinearPolar2D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double LinearSpherical3D_Cell_surface(LinearSpherical3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+// Triangular
+
+double TriangularCartesian2D_Cell_surface(TriangularCartesian2D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double TriangularCartesian3D_Cell_surface(TriangularCartesian3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double TriangularCylindrical3D_Cell_surface(TriangularCylindrical3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double TriangularPolar2D_Cell_surface(TriangularPolar2D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double TriangularSpherical3D_Cell_surface(TriangularSpherical3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+// Quadrilateral
+
+double QuadrilateralCartesian2D_Cell_surface(QuadrilateralCartesian2D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double QuadrilateralCartesian3D_Cell_surface(QuadrilateralCartesian3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double QuadrilateralCylindrical3D_Cell_surface(QuadrilateralCylindrical3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double QuadrilateralPolar2D_Cell_surface(QuadrilateralPolar2D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double QuadrilateralSpherical3D_Cell_surface(QuadrilateralSpherical3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+// Tetrahedral
+
+double TetrahedralCartesian3D_Cell_surface(TetrahedralCartesian3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double TetrahedralCylindrical3D_Cell_surface(TetrahedralCylindrical3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
+
+double TetrahedralSpherical3D_Cell_surface(TetrahedralSpherical3D_Cell cell)
+{
+	return viennagrid::surface(cell.get_cell());
+}
