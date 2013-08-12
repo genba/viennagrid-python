229fa9a9a15c7a46f73d71dafcdfabb535498b07
Jonan CM <jonancm@gmail.com>
2013-08-12 13:42:52 +0100

    Implement missing triangular and tetrahedral edges, needed for 'refine'.

--- src/edges/triangular.cpp
+++ src/edges/triangular.cpp
@@ -13,3 +13,59 @@ TriangularCartesian2D_Edge_t & TriangularCartesian2D_Edge::get_edge()
 {
 	return *edge;
 }
+
+////////////////////////////////
+// TriangularCartesian3D_Edge //
+////////////////////////////////
+
+TriangularCartesian3D_Edge::TriangularCartesian3D_Edge(TriangularCartesian3D_Edge_t &initial_edge)
+{
+	edge = &initial_edge;
+}
+
+TriangularCartesian3D_Edge_t & TriangularCartesian3D_Edge::get_edge()
+{
+	return *edge;
+}
+
+//////////////////////////////////
+// TriangularCylindrical3D_Edge //
+//////////////////////////////////
+
+TriangularCylindrical3D_Edge::TriangularCylindrical3D_Edge(TriangularCylindrical3D_Edge_t &initial_edge)
+{
+	edge = &initial_edge;
+}
+
+TriangularCylindrical3D_Edge_t & TriangularCylindrical3D_Edge::get_edge()
+{
+	return *edge;
+}
+
+////////////////////////////
+// TriangularPolar2D_Edge //
+////////////////////////////
+
+TriangularPolar2D_Edge::TriangularPolar2D_Edge(TriangularPolar2D_Edge_t &initial_edge)
+{
+	edge = &initial_edge;
+}
+
+TriangularPolar2D_Edge_t & TriangularPolar2D_Edge::get_edge()
+{
+	return *edge;
+}
+
+////////////////////////////////
+// TriangularSpherical3D_Edge //
+////////////////////////////////
+
+TriangularSpherical3D_Edge::TriangularSpherical3D_Edge(TriangularSpherical3D_Edge_t &initial_edge)
+{
+	edge = &initial_edge;
+}
+
+TriangularSpherical3D_Edge_t & TriangularSpherical3D_Edge::get_edge()
+{
+	return *edge;
+}
--- src/edges/triangular.hpp
+++ src/edges/triangular.hpp
@@ -15,4 +15,52 @@ public:
 	TriangularCartesian2D_Edge_t & get_edge();
 };
 
+////////////////////////////////
+// TriangularCartesian3D_Edge //
+////////////////////////////////
+
+class TriangularCartesian3D_Edge
+{
+	TriangularCartesian3D_Edge_t    *edge;
+public:
+	TriangularCartesian3D_Edge(TriangularCartesian3D_Edge_t &initial_edge);
+	TriangularCartesian3D_Edge_t & get_edge();
+};
+
+//////////////////////////////////
+// TriangularCylindrical3D_Edge //
+//////////////////////////////////
+
+class TriangularCylindrical3D_Edge
+{
+	TriangularCylindrical3D_Edge_t    *edge;
+public:
+	TriangularCylindrical3D_Edge(TriangularCylindrical3D_Edge_t &initial_edge);
+	TriangularCylindrical3D_Edge_t & get_edge();
+};
+
+////////////////////////////
+// TriangularPolar2D_Edge //
+////////////////////////////
+
+class TriangularPolar2D_Edge
+{
+	TriangularPolar2D_Edge_t    *edge;
+public:
+	TriangularPolar2D_Edge(TriangularPolar2D_Edge_t &initial_edge);
+	TriangularPolar2D_Edge_t & get_edge();
+};
+
+////////////////////////////////
+// TriangularSpherical3D_Edge //
+////////////////////////////////
+
+class TriangularSpherical3D_Edge
+{
+	TriangularSpherical3D_Edge_t    *edge;
+public:
+	TriangularSpherical3D_Edge(TriangularSpherical3D_Edge_t &initial_edge);
+	TriangularSpherical3D_Edge_t & get_edge();
+};
+
 #endif /* end of include guard: EDGES_TRIANGULAR_HPP */
