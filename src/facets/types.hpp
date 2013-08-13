3ba9ee4f4965424fbf901f9c951f463e45fae859
Jonan CM <jonancm@gmail.com>
2013-08-13 09:56:11 +0100

    Fix code alignment.

--- src/facets/types.hpp
+++ src/facets/types.hpp
@@ -7,11 +7,11 @@
  * Triangular *
  **************/
 
-typedef viennagrid::result_of::facet<TriangularCartesian2D_Domain_t>::type       TriangularCartesian2D_Facet_t;
-typedef viennagrid::result_of::facet<TriangularCartesian3D_Domain_t>::type       TriangularCartesian3D_Facet_t;
-typedef viennagrid::result_of::facet<TriangularCylindrical3D_Domain_t>::type     TriangularCylindrical3D_Facet_t;
-typedef viennagrid::result_of::facet<TriangularPolar2D_Domain_t>::type           TriangularPolar2D_Facet_t;
-typedef viennagrid::result_of::facet<TriangularSpherical3D_Domain_t>::type       TriangularSpherical3D_Facet_t;
+typedef viennagrid::result_of::facet<TriangularCartesian2D_Domain_t>::type          TriangularCartesian2D_Facet_t;
+typedef viennagrid::result_of::facet<TriangularCartesian3D_Domain_t>::type          TriangularCartesian3D_Facet_t;
+typedef viennagrid::result_of::facet<TriangularCylindrical3D_Domain_t>::type        TriangularCylindrical3D_Facet_t;
+typedef viennagrid::result_of::facet<TriangularPolar2D_Domain_t>::type              TriangularPolar2D_Facet_t;
+typedef viennagrid::result_of::facet<TriangularSpherical3D_Domain_t>::type          TriangularSpherical3D_Facet_t;
 
 /*****************
  * Quadrilateral *
@@ -27,8 +27,8 @@ typedef viennagrid::result_of::facet<QuadrilateralSpherical3D_Domain_t>::type   
  * Tetrahedral *
  ***************/
 
-typedef viennagrid::result_of::facet<TetrahedralCartesian3D_Domain_t>::type      TetrahedralCartesian3D_Facet_t;
-typedef viennagrid::result_of::facet<TetrahedralCylindrical3D_Domain_t>::type    TetrahedralCylindrical3D_Facet_t;
-typedef viennagrid::result_of::facet<TetrahedralSpherical3D_Domain_t>::type      TetrahedralSpherical3D_Facet_t;
+typedef viennagrid::result_of::facet<TetrahedralCartesian3D_Domain_t>::type         TetrahedralCartesian3D_Facet_t;
+typedef viennagrid::result_of::facet<TetrahedralCylindrical3D_Domain_t>::type       TetrahedralCylindrical3D_Facet_t;
+typedef viennagrid::result_of::facet<TetrahedralSpherical3D_Domain_t>::type         TetrahedralSpherical3D_Facet_t;
 
 #endif /* end of include guard: FACETS_TYPES_HPP */
