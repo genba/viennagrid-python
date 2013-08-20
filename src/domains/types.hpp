e65c35505a77b7af787fbe298a1e826648bb7a72
Jonan CM <jonancm@gmail.com>
2013-08-20 10:54:25 +0100

    Document domain config classes.

--- src/domains/types.hpp
+++ src/domains/types.hpp
@@ -18,7 +18,7 @@
  * LINEAR MESH *
  ***************/
 
-// Line, cartesian 1D
+/// Wrapped domain configuration class for cartesian 1D domains of lines
 struct LinearCartesian1D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
 	                                                          PointCartesian1D_t,
@@ -26,7 +26,7 @@ struct LinearCartesian1D_DomainConfig {
 };
 typedef viennagrid::domain_t<LinearCartesian1D_DomainConfig>                                         LinearCartesian1D_Domain_t;
 
-// Line, cartesian 2D
+/// Wrapped domain configuration class for cartesian 2D domains of lines
 struct LinearCartesian2D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
 	                                                          PointCartesian2D_t,
@@ -34,7 +34,7 @@ struct LinearCartesian2D_DomainConfig {
 };
 typedef viennagrid::domain_t<LinearCartesian2D_DomainConfig>                                         LinearCartesian2D_Domain_t;
 
-// Line, cartesian 3D
+/// Wrapped domain configuration class for cartesian 2D domains of lines
 struct LinearCartesian3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
 	                                                          PointCartesian3D_t,
@@ -43,6 +43,7 @@ struct LinearCartesian3D_DomainConfig {
 typedef viennagrid::domain_t<LinearCartesian3D_DomainConfig>                                         LinearCartesian3D_Domain_t;
 
 // Line, cylindrical (3D)
+/// Wrapped domain configuration class for cartesian 1D domains of lines
 struct LinearCylindrical3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
 	                                                          PointCylindrical_t,
@@ -51,6 +52,7 @@ struct LinearCylindrical3D_DomainConfig {
 typedef viennagrid::domain_t<LinearCylindrical3D_DomainConfig>                                       LinearCylindrical3D_Domain_t;
 
 // Line, polar (2D)
+/// Wrapped domain configuration class for cartesian 1D domains of lines
 struct LinearPolar2D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
 	                                                          PointPolar_t,
@@ -59,6 +61,7 @@ struct LinearPolar2D_DomainConfig {
 typedef viennagrid::domain_t<LinearPolar2D_DomainConfig>                                             LinearPolar2D_Domain_t;
 
 // Line, spherical (3D)
+/// Wrapped domain configuration class for cartesian 1D domains of lines
 struct LinearSpherical3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::line_tag,
 	                                                          PointSpherical_t,
@@ -70,7 +73,7 @@ typedef viennagrid::domain_t<LinearSpherical3D_DomainConfig>                    
  * TRIANGULAR MESH *
  *******************/
 
-// Triangle, cartesian 2D
+/// Wrapped domain configuration class for cartesian 2D domains of triangles
 struct TriangularCartesian2D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::triangle_tag,
 	                                                          PointCartesian2D_t,
@@ -78,7 +81,7 @@ struct TriangularCartesian2D_DomainConfig {
 };
 typedef viennagrid::domain_t<TriangularCartesian2D_DomainConfig>                                     TriangularCartesian2D_Domain_t;
 
-// Triangle, cartesian 3D
+/// Wrapped domain configuration class for cartesian 3D domains of triangles
 struct TriangularCartesian3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::triangle_tag,
 	                                                          PointCartesian3D_t,
@@ -86,7 +89,7 @@ struct TriangularCartesian3D_DomainConfig {
 };
 typedef viennagrid::domain_t<TriangularCartesian3D_DomainConfig>                                     TriangularCartesian3D_Domain_t;
 
-// Triangle, cylindrical (3D)
+/// Wrapped domain configuration class for cylindrical 3D domains of triangles
 struct TriangularCylindrical3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::triangle_tag,
 	                                                          PointCylindrical_t,
@@ -94,7 +97,7 @@ struct TriangularCylindrical3D_DomainConfig {
 };
 typedef viennagrid::domain_t<TriangularCylindrical3D_DomainConfig>                                   TriangularCylindrical3D_Domain_t;
 
-// Triangle, polar (2D)
+/// Wrapped domain configuration class for polar 2D domains of triangles
 struct TriangularPolar2D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::triangle_tag,
 	                                                          PointPolar_t,
@@ -102,7 +105,7 @@ struct TriangularPolar2D_DomainConfig {
 };
 typedef viennagrid::domain_t<TriangularPolar2D_DomainConfig>                                         TriangularPolar2D_Domain_t;
 
-// Triangle, spherical (3D)
+/// Wrapped domain configuration class for spherical 3D domains of triangles
 struct TriangularSpherical3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::triangle_tag,
 	                                                          PointSpherical_t,
@@ -114,35 +117,39 @@ typedef viennagrid::domain_t<TriangularSpherical3D_DomainConfig>                
  * QUADRILATERAL MESH *
  **********************/
 
-// Quadrilateral, cartesian 2D
+/// Wrapped domain configuration class for cartesian 2D domains of quadrilaterals
 struct QuadrilateralCartesian2D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::quadrilateral_tag,
 	                                                          PointCartesian2D_t,
 	                                                          viennagrid::storage::id_handle_tag>::type    type;
 };
 typedef viennagrid::domain_t<QuadrilateralCartesian2D_DomainConfig>                                  QuadrilateralCartesian2D_Domain_t;
-// Quadrilateral, cartesian 3D
+
+/// Wrapped domain configuration class for cartesian 3D domains of quadrilaterals
 struct QuadrilateralCartesian3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::quadrilateral_tag,
 	                                                          PointCartesian3D_t,
 	                                                          viennagrid::storage::id_handle_tag>::type    type;
 };
 typedef viennagrid::domain_t<QuadrilateralCartesian3D_DomainConfig>                                  QuadrilateralCartesian3D_Domain_t;
-// Quadrilateral, cylindrical (3D)
+
+/// Wrapped domain configuration class for cylindrical 3D domains of quadrilaterals
 struct QuadrilateralCylindrical3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::quadrilateral_tag,
 	                                                          PointCylindrical_t,
 	                                                          viennagrid::storage::id_handle_tag>::type    type;
 };
 typedef viennagrid::domain_t<QuadrilateralCylindrical3D_DomainConfig>                                QuadrilateralCylindrical3D_Domain_t;
-// Quadrilateral, polar (2D)
+
+/// Wrapped domain configuration class for polar 2D domains of quadrilaterals
 struct QuadrilateralPolar2D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::quadrilateral_tag,
 	                                                          PointPolar_t,
 	                                                          viennagrid::storage::id_handle_tag>::type    type;
 };
 typedef viennagrid::domain_t<QuadrilateralPolar2D_DomainConfig>                                      QuadrilateralPolar2D_Domain_t;
-// Quadrilateral, spherical (3D)
+
+/// Wrapped domain configuration class for spherical 3D domains of quadrilaterals
 struct QuadrilateralSpherical3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::quadrilateral_tag,
 	                                                          PointSpherical_t,
@@ -154,21 +161,23 @@ typedef viennagrid::domain_t<QuadrilateralSpherical3D_DomainConfig>             
  * TETRAHEDRAL MESH *
  ********************/
 
-// Tetrahedral, cartesian 3D
+/// Wrapped domain configuration class for cartesian 3D domains of tetrahedra
 struct TetrahedralCartesian3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::tetrahedron_tag,
 	                                                          PointCartesian3D_t,
 	                                                          viennagrid::storage::id_handle_tag>::type    type;
 };
 typedef viennagrid::domain_t<TetrahedralCartesian3D_DomainConfig>                                    TetrahedralCartesian3D_Domain_t;
-// Tetrahedral, cylindrical (3D)
+
+/// Wrapped domain configuration class for cylindrical 3D domains of tetrahedra
 struct TetrahedralCylindrical3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::tetrahedron_tag,
 	                                                          PointCylindrical_t,
 	                                                          viennagrid::storage::id_handle_tag>::type    type;
 };
 typedef viennagrid::domain_t<TetrahedralCylindrical3D_DomainConfig>                                  TetrahedralCylindrical3D_Domain_t;
-// Tetrahedral, spherical (3D)
+
+/// Wrapped domain configuration class for spherical 3D domains of tetrahedra
 struct TetrahedralSpherical3D_DomainConfig {
 	typedef viennagrid::config::result_of::full_domain_config<viennagrid::tetrahedron_tag,
 	                                                          PointSpherical_t,
