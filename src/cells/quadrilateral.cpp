86618dfc21007237ec96d2db1f4cf8920fb9e404
Jonan CM <jonancm@gmail.com>
2013-08-29 12:55:40 +0100

    Implement method 'get_facets' to get a list of the facets of a quadrilateral cell.

--- src/cells/quadrilateral.cpp
+++ src/cells/quadrilateral.cpp
@@ -1,6 +1,7 @@
 #include "quadrilateral.hpp"
 
 #include "../vertices/quadrilateral.hpp"
+#include "../facets/quadrilateral.hpp"
 
 /////////////////////////////////
 // Quadrilateral, cartesian 2D //
@@ -33,6 +34,25 @@ list QuadrilateralCartesian2D_Cell::get_vertices()
 	}
 	
 	return vertices;
+}
+
+list QuadrilateralCartesian2D_Cell::get_facets()
+{
+	list facets;
+	
+	typedef QuadrilateralCartesian2D_Cell_t                             CellType;
+	typedef viennagrid::result_of::facet_range<CellType>::type          FacetOnCellRange;
+	typedef viennagrid::result_of::iterator<FacetOnCellRange>::type     FacetOnCellIterator;
+	
+	FacetOnCellRange facets_on_cell = viennagrid::elements(*cell);
+	for (FacetOnCellIterator focit = facets_on_cell.begin();
+	                         focit != facets_on_cell.end();
+	                       ++focit)
+	{
+		facets.append<QuadrilateralCartesian2D_Facet>(QuadrilateralCartesian2D_Facet(*focit));
+	}
+	
+	return facets;
 }
 
 QuadrilateralCartesian2D_Cell_t & QuadrilateralCartesian2D_Cell::get_cell()
@@ -73,6 +93,25 @@ list QuadrilateralCartesian3D_Cell::get_vertices()
 	return vertices;
 }
 
+list QuadrilateralCartesian3D_Cell::get_facets()
+{
+	list facets;
+	
+	typedef QuadrilateralCartesian3D_Cell_t                             CellType;
+	typedef viennagrid::result_of::facet_range<CellType>::type          FacetOnCellRange;
+	typedef viennagrid::result_of::iterator<FacetOnCellRange>::type     FacetOnCellIterator;
+	
+	FacetOnCellRange facets_on_cell = viennagrid::elements(*cell);
+	for (FacetOnCellIterator focit = facets_on_cell.begin();
+	                         focit != facets_on_cell.end();
+	                       ++focit)
+	{
+		facets.append<QuadrilateralCartesian3D_Facet>(QuadrilateralCartesian3D_Facet(*focit));
+	}
+	
+	return facets;
+}
+
 QuadrilateralCartesian3D_Cell_t & QuadrilateralCartesian3D_Cell::get_cell()
 {
 	return *cell;
@@ -109,6 +148,25 @@ list QuadrilateralCylindrical3D_Cell::get_vertices()
 	}
 	
 	return vertices;
+}
+
+list QuadrilateralCylindrical3D_Cell::get_facets()
+{
+	list facets;
+	
+	typedef QuadrilateralCylindrical3D_Cell_t                           CellType;
+	typedef viennagrid::result_of::facet_range<CellType>::type          FacetOnCellRange;
+	typedef viennagrid::result_of::iterator<FacetOnCellRange>::type     FacetOnCellIterator;
+	
+	FacetOnCellRange facets_on_cell = viennagrid::elements(*cell);
+	for (FacetOnCellIterator focit = facets_on_cell.begin();
+	                         focit != facets_on_cell.end();
+	                       ++focit)
+	{
+		facets.append<QuadrilateralCylindrical3D_Facet>(QuadrilateralCylindrical3D_Facet(*focit));
+	}
+	
+	return facets;
 }
 
 QuadrilateralCylindrical3D_Cell_t & QuadrilateralCylindrical3D_Cell::get_cell()
@@ -149,6 +207,25 @@ list QuadrilateralPolar2D_Cell::get_vertices()
 	return vertices;
 }
 
+list QuadrilateralPolar2D_Cell::get_facets()
+{
+	list facets;
+	
+	typedef QuadrilateralPolar2D_Cell_t                                 CellType;
+	typedef viennagrid::result_of::facet_range<CellType>::type          FacetOnCellRange;
+	typedef viennagrid::result_of::iterator<FacetOnCellRange>::type     FacetOnCellIterator;
+	
+	FacetOnCellRange facets_on_cell = viennagrid::elements(*cell);
+	for (FacetOnCellIterator focit = facets_on_cell.begin();
+	                         focit != facets_on_cell.end();
+	                       ++focit)
+	{
+		facets.append<QuadrilateralPolar2D_Facet>(QuadrilateralPolar2D_Facet(*focit));
+	}
+	
+	return facets;
+}
+
 QuadrilateralPolar2D_Cell_t & QuadrilateralPolar2D_Cell::get_cell()
 {
 	return *cell;
@@ -185,6 +262,25 @@ list QuadrilateralSpherical3D_Cell::get_vertices()
 	}
 	
 	return vertices;
+}
+
+list QuadrilateralSpherical3D_Cell::get_facets()
+{
+	list facets;
+	
+	typedef QuadrilateralSpherical3D_Cell_t                             CellType;
+	typedef viennagrid::result_of::facet_range<CellType>::type          FacetOnCellRange;
+	typedef viennagrid::result_of::iterator<FacetOnCellRange>::type     FacetOnCellIterator;
+	
+	FacetOnCellRange facets_on_cell = viennagrid::elements(*cell);
+	for (FacetOnCellIterator focit = facets_on_cell.begin();
+	                         focit != facets_on_cell.end();
+	                       ++focit)
+	{
+		facets.append<QuadrilateralSpherical3D_Facet>(QuadrilateralSpherical3D_Facet(*focit));
+	}
+	
+	return facets;
 }
 
 QuadrilateralSpherical3D_Cell_t & QuadrilateralSpherical3D_Cell::get_cell()
