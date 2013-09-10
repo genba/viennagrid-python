4d03f3fa93812e93108b4cfe0350f28d37108d9a
Jonan CM <jonancm@gmail.com>
2013-09-10 15:57:35 +0100

    Expand tutorial 'io.py' with information on how to use writers.

--- doc/examples/viennagrid_wrapper/io.py
+++ doc/examples/viennagrid_wrapper/io.py
@@ -12,16 +12,24 @@ from __future__ import print_function
 # (in case we want to read segmentation data from the mesh file), and we need the
 # Netgen reader function, too.
 # 
+# In addition, we will then write the read domain and segmentation to a VTK file
+# and also to an OpenDX file. Thus, we will need the VTK writer and the OpenDX writer
+# functions, too.
+# 
 # (Notice that the 'read_netgen' function and all other I/O functions
 # work with any type of domain and segmentation without name change.)
 
 from viennagrid.wrapper import TriangularCartesian3D_Domain as Domain
 from viennagrid.wrapper import TriangularCartesian3D_Segmentation as Segmentation
-from viennagrid.wrapper import read_netgen
+from viennagrid.wrapper import read_netgen, write_vtk, write_opendx
 
 import os
 
 file_path = os.path.join(os.path.split(__file__)[0], '../data/half-trigate.mesh')
+
+#################
+# Netgen reader #
+#################
 
 # In case we want to read only the domain information from the mesh file, we would
 # just create an empty domain and call the Netgen reader on it with the file path
@@ -37,3 +45,26 @@ read_netgen(file_path, domain)
 domain = Domain()
 segmentation = Segmentation(domain)
 read_netgen(file_path, domain, segmentation)
+
+##############
+# VTK writer #
+##############
+
+# In order to write a domain to a VTK file, you just have to call the VTK reader
+# specifying the file path and the domain to be written
+
+write_vtk('output_file.vtu', domain)
+
+# If you want to write the information of both a domain and a segmentation of that
+# domain, you have to specify the segmentation, too:
+
+write_vtk('output_file.vtu', domain, segmentation)
+
+#################
+# OpenDX writer #
+#################
+
+# The OpenDX writer works similarly to the VTK writer, but it doesn't support
+# segmentation, so you can only write the information contained in a domain:
+
+write_opendx('output_opendx_file.out', domain)
