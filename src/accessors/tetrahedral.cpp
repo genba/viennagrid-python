ad243389cadd039c21ad82d0897d81b79090975e
Jonan CM <jonancm@gmail.com>
2013-09-10 17:35:49 +0100

    Implement accessors for tetrahedral vertices and cells.

--- src/accessors/tetrahedral.cpp
+++ src/accessors/tetrahedral.cpp
@@ -1,3 +1,159 @@
 #include "tetrahedral.hpp"
 
+/********************
+ * VERTEX ACCESSORS *
+ ********************/
 
+///////////////////////////////////////
+// TetrahedralCartesian3D_Vertex_Accessor //
+///////////////////////////////////////
+
+TetrahedralCartesian3D_Vertex_Accessor::TetrahedralCartesian3D_Vertex_Accessor()
+{
+	accessor = new TetrahedralCartesian3D_Vertex_Accessor_t(container);
+}
+
+TetrahedralCartesian3D_Vertex_Accessor::~TetrahedralCartesian3D_Vertex_Accessor()
+{
+	delete accessor;
+	// TODO: Is this needed or does Python handle it with the garbage collector as in the other classes?
+}
+
+double TetrahedralCartesian3D_Vertex_Accessor::get_value(TetrahedralCartesian3D_Vertex &vertex)
+{
+	return (*accessor)(vertex.get_vertex());
+}
+
+void TetrahedralCartesian3D_Vertex_Accessor::set_value(TetrahedralCartesian3D_Vertex &vertex, double new_value)
+{
+	(*accessor)(vertex.get_vertex()) = new_value;
+}
+
+/////////////////////////////////////////
+// TetrahedralCylindrical3D_Vertex_Accessor //
+/////////////////////////////////////////
+
+TetrahedralCylindrical3D_Vertex_Accessor::TetrahedralCylindrical3D_Vertex_Accessor()
+{
+	accessor = new TetrahedralCylindrical3D_Vertex_Accessor_t(container);
+}
+
+TetrahedralCylindrical3D_Vertex_Accessor::~TetrahedralCylindrical3D_Vertex_Accessor()
+{
+	delete accessor;
+	// TODO: Is this needed or does Python handle it with the garbage collector as in the other classes?
+}
+
+double TetrahedralCylindrical3D_Vertex_Accessor::get_value(TetrahedralCylindrical3D_Vertex &vertex)
+{
+	return (*accessor)(vertex.get_vertex());
+}
+
+void TetrahedralCylindrical3D_Vertex_Accessor::set_value(TetrahedralCylindrical3D_Vertex &vertex, double new_value)
+{
+	(*accessor)(vertex.get_vertex()) = new_value;
+}
+
+///////////////////////////////////////
+// TetrahedralSpherical3D_Vertex_Accessor //
+///////////////////////////////////////
+
+TetrahedralSpherical3D_Vertex_Accessor::TetrahedralSpherical3D_Vertex_Accessor()
+{
+	accessor = new TetrahedralSpherical3D_Vertex_Accessor_t(container);
+}
+
+TetrahedralSpherical3D_Vertex_Accessor::~TetrahedralSpherical3D_Vertex_Accessor()
+{
+	delete accessor;
+	// TODO: Is this needed or does Python handle it with the garbage collector as in the other classes?
+}
+
+double TetrahedralSpherical3D_Vertex_Accessor::get_value(TetrahedralSpherical3D_Vertex &vertex)
+{
+	return (*accessor)(vertex.get_vertex());
+}
+
+void TetrahedralSpherical3D_Vertex_Accessor::set_value(TetrahedralSpherical3D_Vertex &vertex, double new_value)
+{
+	(*accessor)(vertex.get_vertex()) = new_value;
+}
+
+/******************
+ * CELL ACCESSORS *
+ ******************/
+
+/////////////////////////////////////
+// TetrahedralCartesian3D_Cell_Accessor //
+/////////////////////////////////////
+
+TetrahedralCartesian3D_Cell_Accessor::TetrahedralCartesian3D_Cell_Accessor()
+{
+	accessor = new TetrahedralCartesian3D_Cell_Accessor_t(container);
+}
+
+TetrahedralCartesian3D_Cell_Accessor::~TetrahedralCartesian3D_Cell_Accessor()
+{
+	delete accessor;
+	// TODO: Is this needed or does Python handle it with the garbage collector as in the other classes?
+}
+
+double TetrahedralCartesian3D_Cell_Accessor::get_value(TetrahedralCartesian3D_Cell &cell)
+{
+	return (*accessor)(cell.get_cell());
+}
+
+void TetrahedralCartesian3D_Cell_Accessor::set_value(TetrahedralCartesian3D_Cell &cell, double new_value)
+{
+	(*accessor)(cell.get_cell()) = new_value;
+}
+
+///////////////////////////////////////
+// TetrahedralCylindrical3D_Cell_Accessor //
+///////////////////////////////////////
+
+TetrahedralCylindrical3D_Cell_Accessor::TetrahedralCylindrical3D_Cell_Accessor()
+{
+	accessor = new TetrahedralCylindrical3D_Cell_Accessor_t(container);
+}
+
+TetrahedralCylindrical3D_Cell_Accessor::~TetrahedralCylindrical3D_Cell_Accessor()
+{
+	delete accessor;
+	// TODO: Is this needed or does Python handle it with the garbage collector as in the other classes?
+}
+
+double TetrahedralCylindrical3D_Cell_Accessor::get_value(TetrahedralCylindrical3D_Cell &cell)
+{
+	return (*accessor)(cell.get_cell());
+}
+
+void TetrahedralCylindrical3D_Cell_Accessor::set_value(TetrahedralCylindrical3D_Cell &cell, double new_value)
+{
+	(*accessor)(cell.get_cell()) = new_value;
+}
+
+/////////////////////////////////////
+// TetrahedralSpherical3D_Cell_Accessor //
+/////////////////////////////////////
+
+TetrahedralSpherical3D_Cell_Accessor::TetrahedralSpherical3D_Cell_Accessor()
+{
+	accessor = new TetrahedralSpherical3D_Cell_Accessor_t(container);
+}
+
+TetrahedralSpherical3D_Cell_Accessor::~TetrahedralSpherical3D_Cell_Accessor()
+{
+	delete accessor;
+	// TODO: Is this needed or does Python handle it with the garbage collector as in the other classes?
+}
+
+double TetrahedralSpherical3D_Cell_Accessor::get_value(TetrahedralSpherical3D_Cell &cell)
+{
+	return (*accessor)(cell.get_cell());
+}
+
+void TetrahedralSpherical3D_Cell_Accessor::set_value(TetrahedralSpherical3D_Cell &cell, double new_value)
+{
+	(*accessor)(cell.get_cell()) = new_value;
+}
