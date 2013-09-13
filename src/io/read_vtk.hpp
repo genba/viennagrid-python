a7f1a03119865590bd1a67460c0eda5459edaeaf
Jonan CM <jonancm@gmail.com>
2013-09-13 14:48:44 +0100

    Fix 'ArgumentError' due to 'read_vtk'.
    Since the signatures of the Python call and the C++ function didn't match, Boost.Python raised an 'ArgumentError' whenever 'read_vtk' was used. With the changes of this commit, this bug is fixed.

--- src/io/read_vtk.hpp
+++ src/io/read_vtk.hpp
@@ -14,18 +14,16 @@
 #include <boost/python.hpp>
 using namespace boost::python;
 
-#include <string>
-
 ////////////
 // LINEAR //
 ////////////
 
-void LinearCartesian1D_Domain_read_vtk(std::string const &filename, LinearCartesian1D_Domain *domain, LinearCartesian1D_Segmentation *segmentation = NULL, dict accessors = dict());
-void LinearCartesian2D_Domain_read_vtk(std::string const &filename, LinearCartesian2D_Domain *domain, LinearCartesian2D_Segmentation *segmentation = NULL, dict accessors = dict());
-void LinearCartesian3D_Domain_read_vtk(std::string const &filename, LinearCartesian3D_Domain *domain, LinearCartesian3D_Segmentation *segmentation = NULL, dict accessors = dict());
-void LinearCylindrical3D_Domain_read_vtk(std::string const &filename, LinearCylindrical3D_Domain *domain, LinearCylindrical3D_Segmentation *segmentation = NULL, dict accessors = dict());
-void LinearPolar2D_Domain_read_vtk(std::string const &filename, LinearPolar2D_Domain *domain, LinearPolar2D_Segmentation *segmentation = NULL, dict accessors = dict());
-void LinearSpherical3D_Domain_read_vtk(std::string const &filename, LinearSpherical3D_Domain *domain, LinearSpherical3D_Segmentation *segmentation = NULL, dict accessors = dict());
+void LinearCartesian1D_Domain_read_vtk(str filename, LinearCartesian1D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void LinearCartesian2D_Domain_read_vtk(str filename, LinearCartesian2D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void LinearCartesian3D_Domain_read_vtk(str filename, LinearCartesian3D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void LinearCylindrical3D_Domain_read_vtk(str filename, LinearCylindrical3D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void LinearPolar2D_Domain_read_vtk(str filename, LinearPolar2D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void LinearSpherical3D_Domain_read_vtk(str filename, LinearSpherical3D_Domain domain, object arg1 = object(), dict arg2 = dict());
 
 BOOST_PYTHON_FUNCTION_OVERLOADS(LinearCartesian1D_Domain_read_vtk_overloads,
                                 LinearCartesian1D_Domain_read_vtk, 2, 4);
@@ -49,11 +47,11 @@ BOOST_PYTHON_FUNCTION_OVERLOADS(LinearSpherical3D_Domain_read_vtk_overloads,
 // TRIANGULAR //
 ////////////////
 
-void TriangularCartesian2D_Domain_read_vtk(std::string const &filename, TriangularCartesian2D_Domain *domain, TriangularCartesian2D_Segmentation *segmentation = NULL, dict accessors = dict());
-void TriangularCartesian3D_Domain_read_vtk(std::string const &filename, TriangularCartesian3D_Domain *domain, TriangularCartesian3D_Segmentation *segmentation = NULL, dict accessors = dict());
-void TriangularCylindrical3D_Domain_read_vtk(std::string const &filename, TriangularCylindrical3D_Domain *domain, TriangularCylindrical3D_Segmentation *segmentation = NULL, dict accessors = dict());
-void TriangularPolar2D_Domain_read_vtk(std::string const &filename, TriangularPolar2D_Domain *domain, TriangularPolar2D_Segmentation *segmentation = NULL, dict accessors = dict());
-void TriangularSpherical3D_Domain_read_vtk(std::string const &filename, TriangularSpherical3D_Domain *domain, TriangularSpherical3D_Segmentation *segmentation = NULL, dict accessors = dict());
+void TriangularCartesian2D_Domain_read_vtk(str filename, TriangularCartesian2D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void TriangularCartesian3D_Domain_read_vtk(str filename, TriangularCartesian3D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void TriangularCylindrical3D_Domain_read_vtk(str filename, TriangularCylindrical3D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void TriangularPolar2D_Domain_read_vtk(str filename, TriangularPolar2D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void TriangularSpherical3D_Domain_read_vtk(str filename, TriangularSpherical3D_Domain domain, object arg1 = object(), dict arg2 = dict());
 
 BOOST_PYTHON_FUNCTION_OVERLOADS(TriangularCartesian2D_Domain_read_vtk_overloads,
                                 TriangularCartesian2D_Domain_read_vtk, 2, 4);
@@ -74,11 +72,11 @@ BOOST_PYTHON_FUNCTION_OVERLOADS(TriangularSpherical3D_Domain_read_vtk_overloads,
 // QUADRILATERAL //
 ///////////////////
 
-void QuadrilateralCartesian2D_Domain_read_vtk(std::string const &filename, QuadrilateralCartesian2D_Domain *domain, QuadrilateralCartesian2D_Segmentation *segmentation = NULL, dict accessors = dict());
-void QuadrilateralCartesian3D_Domain_read_vtk(std::string const &filename, QuadrilateralCartesian3D_Domain *domain, QuadrilateralCartesian3D_Segmentation *segmentation = NULL, dict accessors = dict());
-void QuadrilateralCylindrical3D_Domain_read_vtk(std::string const &filename, QuadrilateralCylindrical3D_Domain *domain, QuadrilateralCylindrical3D_Segmentation *segmentation = NULL, dict accessors = dict());
-void QuadrilateralPolar2D_Domain_read_vtk(std::string const &filename, QuadrilateralPolar2D_Domain *domain, QuadrilateralPolar2D_Segmentation *segmentation = NULL, dict accessors = dict());
-void QuadrilateralSpherical3D_Domain_read_vtk(std::string const &filename, QuadrilateralSpherical3D_Domain *domain, QuadrilateralSpherical3D_Segmentation *segmentation = NULL, dict accessors = dict());
+void QuadrilateralCartesian2D_Domain_read_vtk(str filename, QuadrilateralCartesian2D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void QuadrilateralCartesian3D_Domain_read_vtk(str filename, QuadrilateralCartesian3D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void QuadrilateralCylindrical3D_Domain_read_vtk(str filename, QuadrilateralCylindrical3D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void QuadrilateralPolar2D_Domain_read_vtk(str filename, QuadrilateralPolar2D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void QuadrilateralSpherical3D_Domain_read_vtk(str filename, QuadrilateralSpherical3D_Domain domain, object arg1 = object(), dict arg2 = dict());
 
 BOOST_PYTHON_FUNCTION_OVERLOADS(QuadrilateralCartesian2D_Domain_read_vtk_overloads,
                                 QuadrilateralCartesian2D_Domain_read_vtk, 2, 4);
@@ -99,9 +97,9 @@ BOOST_PYTHON_FUNCTION_OVERLOADS(QuadrilateralSpherical3D_Domain_read_vtk_overloa
 // TETRAHEDRAL //
 /////////////////
 
-void TetrahedralCartesian3D_Domain_read_vtk(std::string const &filename, TetrahedralCartesian3D_Domain *domain, TetrahedralCartesian3D_Segmentation *segmentation = NULL, dict accessors = dict());
-void TetrahedralCylindrical3D_Domain_read_vtk(std::string const &filename, TetrahedralCylindrical3D_Domain *domain, TetrahedralCylindrical3D_Segmentation *segmentation = NULL, dict accessors = dict());
-void TetrahedralSpherical3D_Domain_read_vtk(std::string const &filename, TetrahedralSpherical3D_Domain *domain, TetrahedralSpherical3D_Segmentation *segmentation = NULL, dict accessors = dict());
+void TetrahedralCartesian3D_Domain_read_vtk(str filename, TetrahedralCartesian3D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void TetrahedralCylindrical3D_Domain_read_vtk(str filename, TetrahedralCylindrical3D_Domain domain, object arg1 = object(), dict arg2 = dict());
+void TetrahedralSpherical3D_Domain_read_vtk(str filename, TetrahedralSpherical3D_Domain domain, object arg1 = object(), dict arg2 = dict());
 
 BOOST_PYTHON_FUNCTION_OVERLOADS(TetrahedralCartesian3D_Domain_read_vtk_overloads,
                                 TetrahedralCartesian3D_Domain_read_vtk, 2, 4);
